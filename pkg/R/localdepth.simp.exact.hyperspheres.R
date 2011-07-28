54766d6e1cdf30e8bf46f05eedc998d228cada6b
claudio <claudio@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-28 15:04:26 +0000

    Add functions for the evaluation of the local simplicial depth and simplicial depth for data in a hyperspheres. Functions in ldareashs.f and lddiamshs.f are set but they should be fixed. Fixed a bug in the functions in localdepthsimplicial.f
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localdepth@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/localdepth.simp.exact.hyperspheres.R
@@ -0,0 +1,102 @@
+#############################################################
+#
+#	localdepth.simp.exact.hyperspheres function
+#	Author: Claudio Agostinelli and Mario Romanazzi
+#	E-mail: claudio@unive.it
+#	Date: July, 28, 2011
+#	Version: 0.1
+#
+#	Copyright (C) 2011 Claudio Agostinelli and Mario Romanazzi
+#
+#############################################################
+
+localdepth.simp.exact.hyperspheres <- function(x, y=NULL, tau, use=c('volume', 'diameter'), nsamp='all', nmax=1, tol=0) {
+  warning("x and y must be points in a unitary hyperspheres. Volume and diameter are evaluated on the corresponding euclidean simplex. Please fix me as soon as possible, here and in the quantile.localdepth functioin!")
+  if (is.null(y))
+    y <- x
+  if (is.vector(x))
+    x <- matrix(x, ncol=1)
+  if (is.vector(y))
+    y <- matrix(y, ncol=1)
+  x <- as.matrix(x)
+  y <- as.matrix(y)
+  
+  nx <- nrow(x)
+  ny <- nrow(y)
+  nc <- ncol(x)
+  nt <- choose(nx, nc+1)
+  if (nt > .Machine$integer.max)
+    nt <- .Machine$integer.max
+
+  use <- match.arg(use)
+
+  if (is.numeric(nsamp) && nsamp <= 0) stop("the argument 'nsamp' must be positive")
+  if (is.numeric(nsamp) && nsamp > nt) {
+      warning("Since 'nsamp' is greater than the number of simplex the 'all' method is used")
+      nsamp <- 'all'
+  }
+  if (use=='diameter') nuse=0
+  if (use=='volume') nuse=1
+  
+  if (is.character(nsamp) && nsamp=='all') {
+    z <- .Fortran("ldsehs",
+      as.matrix(x), 
+      as.matrix(y),
+      as.double(tau),
+      as.integer(nc),
+      as.integer(nt),
+      as.integer(nx),
+      as.integer(ny),
+      as.integer(nuse),
+      as.double(tol),
+      depth=double(ny),
+      localdepth=double(ny),
+      PACKAGE = "localdepth")
+  } else if (is.numeric(nsamp)){
+    nt <- round(nt*nmax)
+    z <- .Fortran("ldseahs",
+      as.matrix(x), 
+      as.matrix(y),
+      as.double(tau),
+      as.integer(nc),
+      as.integer(nt),
+      as.integer(nsamp),
+      as.integer(nx),
+      as.integer(ny),
+      as.integer(nuse),
+      as.double(tol),
+      depth=double(ny),
+      localdepth=double(ny),                  
+      nd=double(1),
+      nld=double(1),
+      PACKAGE = "localdepth") 
+  } else {
+    stop("the argument 'nsamp' must be either 'all' or a positive number")
+  }
+  result <- list()
+  if (is.numeric(nsamp)) {
+    #### Sistemare la divisione per il numero di simplessi
+    result$localdepth <- z$localdepth/z$nd
+    result$depth <- z$depth/z$nd
+    result$max.localdepth <- max(result$localdepth)
+    result$max.depth <- max(result$depth)  
+    result$num <- c(z$nd,z$nld)
+  } else {
+    result$localdepth <- z$localdepth/nt
+    result$depth <- z$depth/nt
+    result$max.localdepth <- max(result$localdepth)
+    result$max.depth <- max(result$depth)
+    result$num <- c(nt,nt)
+  }
+  result$call <- match.call()
+  result$tau <- tau
+  result$use <- use
+  result$tol <- tol
+  result$x <- x
+  result$y <- y
+  result$type <- 'exact'
+  result$nsamp <- nsamp
+  result$method <- 'simplicial.hyperspheres'
+  class(result) <- 'localdepth'
+  return(result)
+}
