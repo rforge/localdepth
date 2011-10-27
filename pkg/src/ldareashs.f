2cd0b3b7e0d3e7af80f690c62abbbceac5734a66
claudio <claudio@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-27 13:11:57 +0000

    The functions do the calculation for the area (only in the sphere) and the diameter of simplex
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localdepth@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/ldareashs.f
+++ pkg/src/ldareashs.f
@@ -5,32 +5,32 @@
 
       parameter(dzero=0.0d00)
 
-      dimension darea(nt), x(nrx, nc), xsimplex(nc+1,nc)
-      dimension isimplex(nc+1)
+      dimension darea(nt), x(nrx, nc), xsimplex(nc,nc)
+      dimension isimplex(nc)
 
-      external ldarea
+      external ldareahs
 
 C Inizializza il vettore degli indici degli spigoli dei simplessi
-      do 10 i=1,(nc+1)
+      do 10 i=1,nc
         isimplex(i) = i
  10   continue
       nsimp = 0
-      i = nc+1
-      do 20 while (isimplex(1).le.(nrx-nc))
+      i = nc
+      do 20 while (isimplex(1).le.(nrx-nc+1))
         icont = 1
-        do 30 while (i.lt.(nc+1).and.icont.eq.1)
-          if (isimplex(i).lt.(nrx-nc+i)) then
+        do 30 while (i.lt.nc.and.icont.eq.1)
+          if (isimplex(i).lt.(nrx-nc+1+i)) then
             i = i + 1
           else 
             icont = 0
           endif
  30     continue
-        if (isimplex(i).le.(nrx-nc-1+i)) then
+        if (isimplex(i).le.(nrx-nc+i)) then
 
 CC Evaluate the volume of a given simplex
 CC          write(*,*) isimplex
           nsimp = nsimp+1
-          do 50 ii=1,(nc+1)
+          do 50 ii=1,nc
             is = isimplex(ii)
             do 60 jj=1,nc
               xsimplex(ii,jj) = x(is,jj)
@@ -42,14 +42,48 @@ CC End of Evaluate the volume of a given simplex
         else
           isimplex(i-1) = isimplex(i-1)+1
           j = i
-          do 40 while (j.le.(nc+1))
+          do 40 while (j.le.nc)
             isimplex(j) = isimplex(j-1)+1
             j = j+1
  40       continue
           i = i-1
         endif      
  20   continue
-CC      write(*,*) nsimp
+      return
+      end
+
+      SUBROUTINE areashsa(X, nt, nc, nrx, diam)
+
+      implicit double precision(a-h,o-z)
+      implicit integer (n,i,j,k)
+
+      parameter(dzero=0.0d00)
+
+      dimension diam(nt), x(nrx, nc), xsimplex(nc,nc)
+      dimension isimplex(nrx)
+
+      external ldareahs
+      external rndstart
+      external rndend
+      external rndunif
+
+      call rndstart()
+
+      do 20 nsimp=1,nt
+        do 10 i=1,nrx
+          isimplex(i)=i
+ 10     continue
+        do 50 ii=1,nc
+          is = (nrx-ii) * rndunif()+1
+          iis = isimplex(is)
+          isimplex(is) = isimplex(nrx-ii+1)
+          do 60 jj=1,nc
+            xsimplex(ii,jj) = x(iis,jj)
+ 60       continue
+ 50     continue
+        call ldareahs(xsimplex, nc, diam(nsimp))
+ 20   continue
+      call rndend()
       return
       end
 
@@ -58,30 +92,37 @@ CC      write(*,*) nsimp
       implicit double precision(a-h,o-z)
       implicit integer (n,i,j,k)
 
+      parameter(dzero=0.0d00)
       parameter(duno=1.0d00)
 
-      dimension x(nc+1, nc), xx(nc+1, nc+1)
-      dimension ipvt(nc+1), ztemp(nc+1), dwork(nc+1), ddeth(2)
-
-      external dgeco
-      external dgedi
-      external dgamma
-
-      dc = nc
-
-      do 10 i=1,(nc+1)
-        do 20 j=2,(nc+1)
-          xx(i,j) = x(i,j-1)
+      dimension x(nc, nc)
+      dimension darchi(nc)
+      do 5 i=1,nc
+        darchi(i) = dzero
+ 5    continue
+      dperi = dzero
+      ii = 0
+      do 10 i=1,(nc-1)
+        do 20 j=(i+1),(nc)
+          temp = dzero
+          do 30 k=1,nc
+            temp = temp+x(i,k)*x(j,k) 
+ 30       continue
+          ii = ii + 1
+          darchi(ii) = dacos(temp)
+          if (darchi(ii).lt.dzero) then
+            darchi(ii) = dzero
+          endif
+          dperi= dperi + darchi(ii)
  20     continue
  10   continue
-      do 30 k=1,(nc+1)
-        xx(k,1) = duno
- 30   continue
-
-      call dgeco(xx,(nc+1),(nc+1),ipvt,rcond,ztemp)
-      call dgedi(xx,(nc+1),(nc+1),ipvt,ddeth,dwork,10)	
-
-      darea = dabs(ddeth(1)*(10.0d00**ddeth(2)))/dgamma(dc+duno)
-
+      dtemp = dtan(dperi/4.0d00)
+     &  *dtan((dperi/2.0d00-darchi(1))/2.0d00)
+     &  *dtan((dperi/2.0d00-darchi(2))/2.0d00)
+     &  *dtan((dperi/2.0d00-darchi(3))/2.0d00)
+      if (dtemp.lt.dzero) then
+        dtemp = dzero
+      endif
+      darea = 4.0d00*datan(dsqrt(dtemp))
       return
       end
--- pkg/src/lddiamshs.f
+++ pkg/src/lddiamshs.f
@@ -5,32 +5,32 @@
 
       parameter(dzero=0.0d00)
 
-      dimension diam(nt), x(nrx, nc), xsimplex(nc+1,nc)
-      dimension isimplex(nc+1)
+      dimension diam(nt), x(nrx, nc), xsimplex(nc,nc)
+      dimension isimplex(nc)
 
-      external lddiam
+      external lddiamhs
 
 C Inizializza il vettore degli indici degli spigoli dei simplessi
-      do 10 i=1,(nc+1)
+      do 10 i=1,nc
         isimplex(i) = i
  10   continue
       nsimp = 0
-      i = nc+1
-      do 20 while (isimplex(1).le.(nrx-nc))
+      i = nc
+      do 20 while (isimplex(1).le.(nrx-nc+1))
         icont = 1
-        do 30 while (i.lt.(nc+1).and.icont.eq.1)
-          if (isimplex(i).lt.(nrx-nc+i)) then
+        do 30 while (i.lt.nc.and.icont.eq.1)
+          if (isimplex(i).lt.(nrx-nc+1+i)) then
             i = i + 1
           else 
             icont = 0
           endif
  30     continue
-        if (isimplex(i).le.(nrx-nc-1+i)) then
+        if (isimplex(i).le.(nrx-nc+i)) then
 
 CC Evaluate the diameter of a given simplex
 CC          write(*,*) isimplex
           nsimp = nsimp+1
-          do 50 ii=1,(nc+1)
+          do 50 ii=1,nc
             is = isimplex(ii)
             do 60 jj=1,nc
               xsimplex(ii,jj) = x(is,jj)
@@ -42,14 +42,48 @@ CC End of Evaluate the diamter of a given simplex
         else
           isimplex(i-1) = isimplex(i-1)+1
           j = i
-          do 40 while (j.le.(nc+1))
+          do 40 while (j.le.nc)
             isimplex(j) = isimplex(j-1)+1
             j = j+1
  40       continue
           i = i-1
         endif      
  20   continue
-CC      write(*,*) nsimp
+      return
+      end
+
+      SUBROUTINE diamshsa(X, nt, nc, nrx, diam)
+
+      implicit double precision(a-h,o-z)
+      implicit integer (n,i,j,k)
+
+      parameter(dzero=0.0d00)
+
+      dimension diam(nt), x(nrx, nc), xsimplex(nc,nc)
+      dimension isimplex(nrx)
+
+      external lddiamhs
+      external rndstart
+      external rndend
+      external rndunif
+
+      call rndstart()
+
+      do 20 nsimp=1,nt
+        do 10 i=1,nrx
+          isimplex(i)=i
+ 10     continue
+        do 50 ii=1,nc
+          is = (nrx-ii) * rndunif()+1
+          iis = isimplex(is)
+          isimplex(is) = isimplex(nrx-ii+1)
+          do 60 jj=1,nc
+            xsimplex(ii,jj) = x(iis,jj)
+ 60       continue
+ 50     continue
+        call lddiamhs(xsimplex, nc, diam(nsimp))
+ 20   continue
+      call rndend()
       return
       end
 
@@ -59,23 +93,23 @@ CC      write(*,*) nsimp
       implicit integer (n,i,j,k)
 
       parameter(dzero=0.0d00)
-      parameter(ddue=2.0d00)
-
-      dimension x(nc+1, nc)
+      dimension x(nc, nc)
 
       diam = dzero
-      do 10 i=1,nc
-        do 20 j=(i+1),(nc+1)
-          temp = dzero
+      do 10 i=1,(nc-1)
+        do 20 j=(i+1),(nc)
+          dtemp = dzero
           do 30 k=1,nc
-            temp = temp+(x(i,k)-x(j,k))**ddue 
+            dtemp = dtemp+x(i,k)*x(j,k) 
  30       continue
-          temp = dsqrt(temp)
-          if (diam.le.temp) then
-            diam = temp
+          dtemp = dacos(dtemp)
+          if (diam.le.dtemp) then
+            diam = dtemp
+          endif
+          if (diam.lt.dzero) then
+            diam = dzero
           endif
  20     continue
  10   continue
-
       return
       end
